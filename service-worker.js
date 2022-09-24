5cc8f7fa82ed92877c8c8aa041069e808cde4c09
curly lemon peel <alexpadillarosas@gmail.com>
2022-09-24 19:37:24 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d070adfcbd8d720f28c90e87d2bbd1c3.js"
+  "/precache-manifest.bfe2df9805ecddd9238504820a6160f9.js"
 );
 
 self.addEventListener('message', (event) => {
